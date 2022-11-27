8b52bc5054eb1d611bdc6ac9e138d0b1820da3f9
elppaaa <withbcd@gmail.com>
2022-11-27 14:51:06 +0900

    Initial Commit

---
+++ GithubSearch-TCA/GithubSearch_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearch_TCAApp.swift
+//  GithubSearch-TCA
+//
+//  Created by 송준권 on 2022/11/27.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearch_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
